060422ebe9f068d007444a46a0dfc834c287068e
oren <virtualoren@gmail.com>
2021-01-15 16:40:28 -0600

    Added more remappings to launch file

--- tsdf_package/launch/tsdf_package_launch.py
+++ tsdf_package/launch/tsdf_package_launch.py
@@ -8,14 +8,16 @@ def generate_launch_description():
             node_executable='tsdf_node',
             node_name='tsdf_node',
             remappings=[
-                ("lidar", "/airsim_ros2_wrapper/lidar")
+                ("lidar", "/airsim_ros2_wrapper/lidar"),
+                ("tsdf", "/auto_cinematography/mapping/tsdf"),
+                ("tsdf_occupied_voxels", "/auto_cinematography/debug/markers")
             ],
             parameters=[
                 {
                     "voxel_size" : .5,
                     "truncation_distance" : 4.0,
                     "max_weight" : 10000.0,
-                    "visualize_published_voxels" : True,
+                    "visualize_published_voxels" : False,
                     "publish_distance_squared" : 2500.0,
                     "garbage_collect_distance_squared" : 250000.0,
                     "lidar_frame" : "drone_1/LidarCustom"
